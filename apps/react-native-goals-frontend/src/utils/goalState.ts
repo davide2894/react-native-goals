9122780d36fcc9b4420f150b1a6d8a050bbca14c
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-11-23 20:36:42 +0100

    refactor: delete unused file - goalState.ts

--- apps/react-native-goals-frontend/src/utils/goalState.ts
+++
@@ -1,6 +0,0 @@
-import { goalsReactiveVar } from "../cache";
-import { GoalType } from "../types";
-
-export function updateLocalGoalState(updadetGoals: Array<GoalType>) {
-  goalsReactiveVar(updadetGoals);
-}
