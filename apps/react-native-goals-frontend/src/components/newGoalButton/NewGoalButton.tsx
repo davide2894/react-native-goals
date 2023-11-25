ed30d82f9aa86b9fe779b00b1491a053f4874cd6
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-11-25 23:54:39 +0100

    style: new goal button

--- apps/react-native-goals-frontend/src/components/newGoalButton/NewGoalButton.tsx
+++ apps/react-native-goals-frontend/src/components/newGoalButton/NewGoalButton.tsx
@@ -1,13 +1,17 @@
-import { Alert, Button, View } from "react-native";
+import {
+  Alert,
+  StyleSheet,
+  TouchableHighlight,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { Modal } from "react-native";
 import { useState } from "react";
 import GoalForm from "../goalForm/GoalForm";
+import { caribbeanGreen } from "../../style/globals/color";
 
-function NewGoalButton(
-  {
-    // prop: action to add new goals, received from goalscreen
-  }
-) {
+function NewGoalButton() {
   const [showNewGoalForm, setShowNewGoalForm] = useState(false);
 
   function handlePress(evt) {
@@ -17,7 +21,13 @@ function NewGoalButton(
 
   return (
     <View>
-      <Button onPress={handlePress} title="Add new goal"></Button>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={styles.button}
+        onPress={handlePress}>
+        <Text style={styles.text}>Add</Text>
+      </TouchableOpacity>
+
       {showNewGoalForm && (
         <Modal
           animationType="slide"
@@ -37,3 +47,26 @@ function NewGoalButton(
 }
 
 export default NewGoalButton;
+
+const styles = StyleSheet.create({
+  button: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 40,
+    alignSelf: "center",
+    borderColor: "lightgray",
+    backgroundColor: caribbeanGreen,
+    borderWidth: 1,
+    borderRadius: 10,
+    width: "40%",
+    height: 60,
+    marginBottom: 60,
+    padding: 15,
+  },
+  text: {
+    display: "flex",
+    textAlign: "center",
+    color: "black",
+  },
+});
