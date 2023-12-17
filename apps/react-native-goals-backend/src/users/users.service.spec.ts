8c54362b35512c8709a59937bba08d7a2cbe30c3
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-12-17 19:52:35 +0100

    test: add unit tests for user.service.ts

--- apps/react-native-goals-backend/src/users/users.service.spec.ts
+++ apps/react-native-goals-backend/src/users/users.service.spec.ts
@@ -1,18 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './create-user.dto';
+import { Prisma, User } from '@prisma/client';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let usersService: UsersService;
+  let prismaService: PrismaService;
+  const email = 'testExample123@example.com';
+  const password = 'password123test';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, PrismaService],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    usersService = module.get<UsersService>(UsersService);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  afterEach(async () => {
+    await prismaService.user.deleteMany({
+      where: {
+        email,
+      },
+    });
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(usersService).toBeDefined();
+  });
+
+  describe('method getUserById', () => {
+    it('should be able to find user in DB, given an user id', async () => {
+      const createUserDto: CreateUserDto = {
+        email,
+        password,
+      };
+
+      const newUser = (await prismaService.user.create({
+        data: {
+          email: createUserDto.email,
+          password: password,
+        },
+      })) as User;
+
+      const queryResult = await prismaService.user.findUnique({
+        where: {
+          id: newUser.id,
+        },
+      });
+
+      console.log({ queryResult });
+      expect(queryResult).toBeDefined();
+      expect(queryResult).toHaveProperty('email');
+      // expect(queryResult.email).toEqual(email);
+      // expect(queryResult.id).toEqual(id);
+    });
   });
 });
