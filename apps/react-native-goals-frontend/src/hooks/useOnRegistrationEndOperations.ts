bb4f9c152536b946ed7c6b67056394b10f141292
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-12-18 19:04:49 +0100

    refactor(hook): remove unused file

--- apps/react-native-goals-frontend/src/hooks/useOnRegistrationEndOperations.ts
+++ apps/react-native-goals-frontend/src/hooks/useOnRegistrationEndOperations.ts
@@ -1,9 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import React from "react";
 import { isFirstTimeAccessReactiveVar } from "../cache";
 import { isFirstTimeAccessKey } from "../constants";
 import { saveAccessTokenToStorage } from "../utils/accessToken";
-import apolloClient from "../utils/apolloClient";
 import { saveRefreshTokenToStorage } from "../utils/refreshToken";
 
 async function useOnRegistrationEndOperations(
